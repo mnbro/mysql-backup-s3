5fb402f95513f26f66392e12e64caf7aaaec353c
Mihai Nicolae <mihai.nicolae@protonmail.com>
2023-06-13 20:38:09 +0300

    initial

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# exit if a command fails
+set -eo pipefail
+
+apk update
+
+# install mysqldump
+apk add mysql-client
+apk add mariadb-connector-c
+
+# install gpg
+apk add gnupg
+
+# install s3 tools
+apk add python3 py3-pip
+pip install awscli
+
+# cleanup
+rm -rf /var/cache/apk/*
